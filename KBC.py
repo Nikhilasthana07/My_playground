52975dfe5262c3311fdfd229cfcc6022b90a7ee0
Nikhilasthana07 <nikhilasthana13@gmail.com>
2025-12-05 15:28:58 +0530

    Add KBC file

---
+++ KBC.py
@@ -0,0 +1,177 @@
+#Question function
+def question():
+ sum = 0
+ for i in range(1):
+
+    print("Aapka phala prasn 100 rupye ke liye ye rha computer screen pr.\n")
+    print("Q1) Who developed Python Programming Language?")
+    print("a) Wick van Rossum\nb) Rasmus Lerdorf\nc) Guido van Rossum\nd) Niene stom")
+    ans1 = input()#input the answer
+    print("Coumputer ji",ans1,"pr tala lgaya jaye")
+    if(ans1 == 'c'):#Check the answer is true or flase
+       print("Shi jbab app 100 rupye jeet gye")
+       sum = sum + 100#Calculet the money that player wine
+       print("\n")
+    else:
+       print("Maf kriye aapka uttar galat h.")
+       print("Eska shi uttar hoga c")
+       print("\n")
+
+    print("Aapka dusra prasn 1000 rupye ke liye ye rha computer screen pr.\n")
+    print("Q2) Which type of programming does python support?")
+    print("a) Objesct-Oriented programming\nb) Structured programming\nc) Functional programming\nd) All of the mentioned")
+    ans2 = input()
+    print("Coumputer ji",ans2,"pr tala lgaya jaye")
+    if(ans2 == 'd'):
+       print("Shi jbab app 1000 rupye jeet gye")
+       sum = sum + 1000
+       print("\n")
+    else:
+       print("Maf kriye aapka uttar galat h.")
+       print("Eska shi uttar hoga d")
+       print("\n")
+
+    print("Aapka teesra prasn 10000 rupye ke liye ye rha computer screen pr.\n")
+    print("Q3) Is Python case sensitive when dealing with identifiers?")
+    print("a) No\nb) Yes\nc) Machine dependent\nd) None of the mentioned")
+    ans3 = input()
+    print("Coumputer ji",ans3,"pr tala lgaya jaye")
+    if(ans3 == 'b'):
+       print("Shi jbab app 10000 rupye jeet gye")
+       sum = sum + 10000
+       print("\n")
+    else:
+       print("Maf kriye aapka uttar galat h.")
+       print("Eska shi uttar hoga b")
+       print("\n")
+
+    print("Aapka chautha prasn 25000 rupye ke liye ye rha computer screen pr.\n")
+    print("Q4) Which of the following is the correct extension of the Python file?")
+    print("a) .python\nb) .pl\nc) .py\nd) .p")
+    ans4 = input()
+    print("Coumputer ji",ans4,"pr tala lgaya jaye")
+    if(ans4 == 'c'):
+       print("Shi jbab app 25000 rupye jeet gye")
+       sum = sum + 25000
+       print("\n")
+    else:
+       print("Maf kriye aapka uttar galat h.")
+       print("Eska shi uttar hoga c")
+       print("\n")
+
+    print("Aapka paanchva prasn 50000 rupye ke liye ye rha computer screen pr.\n")
+    print("Q5) All keywords in Python are in?")
+    print("a) Capitalized\nb) Lower case\nc) Upper case\nd) None of the mentioned")
+    ans5 = input()
+    print("Coumputer ji",ans5,"pr tala lgaya jaye")
+    if(ans5 == 'd'):
+       print("Shi jbab app 50000 rupye jeet gye")
+       sum = sum + 50000
+       print("\n")
+    else:
+       print("Maf kriye aapka uttar galat h.")
+       print("Eska shi uttar hoga d")
+       print("\n")
+
+    print("Aapka chhatha prasn 100000 rupye ke liye ye rha computer screen pr.\n")
+    print("Q6) What will be the value of the following Python expression?\n 4 + 3 % 5")
+    print("a) 7\nb) 2\nc) 4\nd) 1")
+    ans6 = input()
+    print("Coumputer ji",ans6,"pr tala lgaya jaye")
+    if(ans6 == 'a'):
+       print("Shi jbab app 100000 rupye jeet gye")
+       sum = sum + 100000
+       print("\n")
+    else:
+       print("Maf kriye aapka uttar galat h.")
+       print("Eska shi uttar hoga a")
+       print("\n")
+
+    print("Aapka satva prasn 1000000 rupye ke liye ye rha computer screen pr.\n")
+    print("Q7) Which of the following is used to define a block of code inPython language?")
+    print("a) Indentation\nb) Key\nc) Brackets\nd) All of the mentioned")
+    ans7 = input()
+    print("Coumputer ji",ans7,"pr tala lgaya jaye")
+    if(ans7 == 'a'):
+       print("Shi jbab app 1000000 rupye jeet gye")
+       sum = sum + 1000000
+       print("\n")
+    else:
+       print("Maf kriye aapka uttar galat h.")
+       print("Eska shi uttar hoga a")
+       print("\n")
+
+    print("Aapka aathva prasn 10000000 rupye ke liye ye rha computer screen pr.\n")
+    print("Q8) Which keyword is used for function in Python language?")
+    print("a) function\nb)def \nc)fun \nd)define ")
+    ans8 = input()
+    print("Coumputer ji",ans8,"pr tala lgaya jaye")
+    if(ans8 == 'b'):
+       print("Shi jbab app 10000000 rupye jeet gye")
+       sum = sum + 10000000
+       print("\n")
+    else:
+       print("Maf kriye aapka uttar galat h.")
+       print("Eska shi uttar hoga b")
+       print("\n")
+
+    print("Aapka navva prasn 50000000 rupye ke liye ye rha computer screen pr.\n")   
+    print("Q9) Which of the following character is used to give single-line comments in Python?") 
+    print("a) //\nb) #\nc) !\nd) /*")
+    ans9 = input()
+    print("Coumputer ji",ans9,"Ko lock kiya jaye")
+    if(ans9 == 'b'):
+       print("Shi jbab app 50000000 rupye jeet gye")
+       sum = sum + 50000000
+       print("\n")
+    else:
+       print("Maf kriye aapka uttar galat h.")
+       print("Eska shi uttar hoga b")
+       print("\n")
+
+    print("Aapka dsva prasn 70000000 rupye ke liye ye rha computer screen pr.\n")
+    print("Q10) What will be the output of the following Python code?\ni = 1\nwhile True:\nif i % 3 == 0:\nbreak\nprint(i)\n i + = 1")
+    print("a) 123\nb)error \nc)12 \nd)Non of the mentioned ")
+    ans10 = input()
+    print("Coumputer ji",ans10,"pr tala lgaya jaye")
+    if(ans10 == 'b'):
+       print(" Shi jbab app 70000000 rupye jeet gye")
+       sum = sum + 70000000
+       print("\n")
+    else:
+       print("Maf kriye aapka uttar galat h.")
+       print("Eska shi uttar hoga b")
+       print("\n")
+
+    print("Aapka total amount hota h =",sum)
+
+    '''print("Q10) Who developed Python Programming Language?")
+    print("a) Wick van Rossum\nb) Rasmus Lerdorf\nc) Guido van Rossum\nd) Niene stom")
+    ans1 = input()
+    print("Coumputer ji",ans1,"Ko lock kriye")
+    if(ans1 == 'c'):
+       print("Shi jbab app 100 rupye jeet gye")
+       print("\n")
+    else:
+       print("Maf kriye aapka uttar galat h.")
+       print("Eska shi uttar hoga c")
+       print("\n")'''
+   
+
+#main function
+name = input("Enter your name: ")
+print("Namaskar",name,"ji Kon Bnega Carore Pati me appka svagat h.\n")
+ready = input("To kya aap khelne ke liye teyar h: ")
+print("\n")
+#Check the condition true or false
+if(('Yes'and 'yes') == ready or ('Ha' and 'ha') == ready):
+    print("TO thih h suru krte h.")
+    question()#calling function
+    
+elif(ready == ('No' and 'no') or ready == ('Na' and 'na')):
+    print("Aap khud exit honge ya dhakke mar kr nikalu.")
+else:
+    print("Jitna puchha ja rha h utna btaiye jada bkchodi nhi")
+
+
+
